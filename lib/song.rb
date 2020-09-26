71432be88ce9887845c009306b2835c58847b8c2
Justice Payne <spikepayne@yahoo.com>
2020-09-26 20:59:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,19 +1,31 @@
 class Song
-  attr_accessor :artist, :name
+
+  attr_accessor :name, :artist
+  
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self 
+  end
+  
+   def self.all 
+    @@all 
   end
 
+  def self.new_by_filename(file_name)
+    split_name = file_name.split(" - ")
+    song = self.new(split_name[1])
+    artist = Artist.new(split_name[0])
+    song.artist = artist
+    artist.songs << song
+    song
+  end
+  
   def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
+    artist = Artist.find_or_create_by_name(name)
+    self.artist = artist
     artist.add_song(self)
   end
 
-  def self.new_by_filename(file)
-    song_info = file.chomp(".mp3").split(" - ")
-    song = Song.new(song_info[1])
-    song.artist_name = song_info[0]
-    song
-  end
 end
