c33c6b90c2860d6d68c40a03432ecef1cc44827d
Justice Payne <spikepayne@yahoo.com>
2020-09-26 21:03:01 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,31 +1,32 @@
-class Song
+class Artist
 
-  attr_accessor :name, :artist
-  
+  attr_accessor :name, :songs
+
   @@all = []
 
   def initialize(name)
     @name = name
-    @@all << self 
-  end
-  
-   def self.all 
-    @@all 
+    @songs = []
+    @@all << self
   end
 
-  def self.new_by_filename(file_name)
-    split_name = file_name.split(" - ")
-    song = self.new(split_name[1])
-    artist = Artist.new(split_name[0])
-    song.artist = artist
-    artist.songs << song
-    song
-  end
-  
-  def artist_name=(name)
-    artist = Artist.find_or_create_by_name(name)
-    self.artist = artist
-    artist.add_song(self)
+  def add_song(song)
+    @songs << song
   end
 
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def print_songs
+    songs.each { |song| puts song.name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.all.detect { |artist| artist.name == name} || Artist.new(name)
+  end
 end
