7a048a5faf00d9dd86cd0976130e7b716a0e1b7e
Justice Payne <spikepayne@yahoo.com>
2020-09-26 20:56:57 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,7 +10,7 @@ class Artist
   end
 
   def add_song(song)
-    self.songs << song  
+    @songs << song  
   end
 
   def save
@@ -21,25 +21,11 @@ class Artist
     @@all
   end
 
-  def self.find_or_create_by_name(name)
-    if self.find(name)
-      self.find(name)
-    else
-      self.create(name)
-    end
-  end
-
-  def self.find(name)
-    self.all.detect { |artist| artist.name == name }
-  end
-
-  def self.create(name)   
-    artist = Artist.new(name)
-    artist.save
-    artist
-  end
-
   def print_songs 
-    self.songs.each {|song| song.name}
+    songs.each { |song| puts song.name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    self.all.detect { |artist| artist.name == name } || Artist.new(name)
   end
 end
