cabfcd053353132543b02abe53d18c490070e0ff
Justice Payne <spikepayne@yahoo.com>
2020-09-26 20:59:19 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -10,8 +10,7 @@ class MP3Importer
   end
 
   def import
-    self.files.each do |filename|
-      Song.new_by_filename(filename)
-    end
+    files.each { |file| Song.new_by_filename(file) }
   end
+  
 end
